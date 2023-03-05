d4855afe499a5599ee1fdd7db263dc6f06eb0b0c
“AxLDvL” <a.denis@it-students.fr>
2023-03-05 16:26:17 +0100

    button reset added and new style

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ let varPouls = document.querySelector('#varPouls');
 let entreePouls= document.querySelector('#entreePouls');
 let sortiePouls = document.querySelector('#sortiePouls');
 
+let refreshButton = document.querySelector('#refresh')
+
 entreeSystolique.oninput = () => variation(entreeSystolique, sortieSystolique,variationSystolique);
 sortieSystolique.oninput = () => variation(entreeSystolique, sortieSystolique,variationSystolique);
 
@@ -19,10 +21,32 @@ sortieDiastolique.oninput = () => variation(entreeDiastolique, sortieDiastolique
 entreePouls.oninput = () => variation(entreePouls, sortiePouls,varPouls);
 sortiePouls.oninput = () => variation(entreePouls, sortiePouls,varPouls);
 
+refreshButton.onclick = () => refresh();
+
 const variation = (entree, sortie , e) => {
     if (entree.value && sortie.value ){
         let result =Math.round(((sortie.value - entree.value)/entree.value)*100 * 100) /100;
         !isNaN(result) ? e.innerHTML = String(result) + "%": e.innerHTML = "erreur";
-       Math.abs(result) <40? e.className = "green" : e.className = "red";
+        (Math.abs(result) <20)? e.className = "green": (Math.abs(result) <40)? e.className = "orange": e.className = "red"
+
     }else e.innerHTML ="";
+}
+const refresh = () => {
+    const inputs =[
+        entreeSystolique,
+        sortieSystolique,
+        entreeDiastolique,
+        sortieDiastolique,
+        entreePouls,
+        sortiePouls
+        ]
+    const variations = [
+        variationSystolique,
+        varDiastolique,
+        varPouls
+    ]
+    inputs.forEach (cell => cell.value = "");
+    variations.forEach(cell => cell.innerHTML = "");
+
+
 }
